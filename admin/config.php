88a60f5f1b005e9c23686b720aff30ecf3bca103
Sergey Kotlyarov <folder.list@gmail.com>
2017-11-06 23:16:44 +0200

    ***

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://hourspace.co.il/admin/');
+define('HTTP_CATALOG', 'http://hourspace.co.il/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://hourspace.co.il/admin/');
+define('HTTPS_CATALOG', 'http://hourspace.co.il/');
+
+// DIR
+define('DIR_APPLICATION', '/home3/hourspac/public_html/admin/');
+define('DIR_SYSTEM', '/home3/hourspac/public_html/system/');
+define('DIR_IMAGE', '/home3/hourspac/public_html/image/');
+define('DIR_LANGUAGE', '/home3/hourspac/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home3/hourspac/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home3/hourspac/public_html/system/config/');
+define('DIR_CACHE', '/home3/hourspac/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home3/hourspac/public_html/system/storage/download/');
+define('DIR_LOGS', '/home3/hourspac/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home3/hourspac/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home3/hourspac/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home3/hourspac/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'hourspac_hourspa');
+define('DB_PASSWORD', 'hourspace');
+define('DB_DATABASE', 'hourspac_hourspace');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
